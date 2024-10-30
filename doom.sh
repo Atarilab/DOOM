05e014d7f44f2688fd2db6840087b2bf0733e0dc
Shafeef Omar <shafeef901@gmail.com>
2024-10-30 13:35:16 +0100

    Updates docker run command to use the host network
    - test successful upon reading lowlevel and highlevel states from inside the docker container

--- doom.sh
+++ doom.sh
@@ -1,10 +1,10 @@
 # Get submodules
 git submodule update --init --recursive
 # Setup Conda
-conda create -n doom python=3.x
-conda activate doom
+conda create -n doom python=3.12
 # Source env vars upon conda activation
 mkdir -p $(conda info --base)/envs/doom/etc/conda/activate.d
-echo "source .env.base" > $(conda info --base)/envs/doom/etc/conda/activate.d/env_vars.sh
+echo "source $PWD/.env.base" > $(conda info --base)/envs/doom/etc/conda/activate.d/env_vars.sh
+conda activate doom
 # Build Image
 docker build -t mujuni-image unitree_mujoco_container/.devcontainer/.
