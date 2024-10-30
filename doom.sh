77b0a63f0a928558a69f829d2f393551d0d9f570
Shafeef Omar <shafeef901@gmail.com>
2024-10-30 15:42:40 +0100

    Mounts DOOM src directory to docker; Adds alias to attach docker terminal to existing container

--- doom.sh
+++ doom.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 conda create -n doom python=3.12
 # Source env vars upon conda activation
 mkdir -p $(conda info --base)/envs/doom/etc/conda/activate.d
-echo "source $PWD/.env.base" > $(conda info --base)/envs/doom/etc/conda/activate.d/env_vars.sh
+echo -e "export DOOM_DIR=$PWD\nsource $PWD/.env.base" > $(conda info --base)/envs/doom/etc/conda/activate.d/env_vars.sh
 conda activate doom
 # Build Image
 docker build -t mujuni-image unitree_mujoco_container/.devcontainer/.
