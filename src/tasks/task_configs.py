0a2c3811df5d3c81fe290f09225ea1f8f7bbb009
Shafeef Omar <shafeef901@gmail.com>
2024-11-25 10:27:54 +0100

    Updates task names

--- src/tasks/task_configs.py
+++ src/tasks/task_configs.py
@@ -1,15 +1,15 @@
 # task_mapping.py
 
 TASK_CONFIG = {
-    "rl-velocity-sim": {
+    "rl-velocity-sim-go2": {
         "controller_config": "controllers/config/rl_velocity_cfg.yaml",
         "robot_interface_config": "robot_interfaces/config/sim_cfg.yaml",
         "robot_config": "robots/go2/config.yaml",
     },
-    "mpc-real": {
+    "mpc-velocity-real-go2": {
         "controller_config": "controllers/config/mpc_controller_cfg.yaml",
         "robot_interface_config": "robot_interfaces/config/real_robot_cfg.yaml",
-        "environment_config": "environments/config/ros2_env_cfg.yaml",
+        "robot_config": "robots/go2/config.yaml",
     },
     # Add more tasks here as needed
 }
