b3b27536577c02087c33e8adc4c48289ea3cd22b
Shafeef Omar <shafeef901@gmail.com>
2025-09-02 22:05:46 +0200

    Adds new task cfg and policies

--- src/tasks/task_configs.py
+++ src/tasks/task_configs.py
@@ -27,6 +27,14 @@ TASK_CONFIG = {
         "controller": "controllers/config/rl_manicont_g1_cfg.yaml",
         "robot_interface": "robot_interfaces/config/real_g1_cfg.yaml",
     },
+    "rl-manicont-sim-g1_upper": {
+        "controller": "controllers/config/rl_manicont_g1_cfg.yaml",
+        "robot_interface": "robot_interfaces/config/sim_box_g1upper_cfg.yaml",
+    },
+    "rl-manicont-real-g1_upper": {
+        "controller": "controllers/config/rl_manicont_g1_cfg.yaml",
+        "robot_interface": "robot_interfaces/config/real_g1_cfg.yaml",
+    },
     # "rl-contact-real-g1": {
     #     "controller": "controllers/config/rl_contact_g1_cfg.yaml",
     #     "robot_interface": "robot_interfaces/config/real_g1_cfg.yaml",
@@ -34,6 +42,14 @@ TASK_CONFIG = {
     "rl-velocity-sim-g1": {
         "controller": "controllers/config/rl_velocity_g1_cfg.yaml",
         "robot_interface": "robot_interfaces/config/sim_flat_g1_cfg.yaml",
+    },
+    "rl-velocity-real-g1": {
+        "controller": "controllers/config/rl_velocity_g1_cfg.yaml",
+        "robot_interface": "robot_interfaces/config/real_g1_cfg.yaml",
+    },
+    "rl-velocity-sim-g1_lower": {
+        "controller": "controllers/config/rl_velocity_g1lower_cfg.yaml",
+        "robot_interface": "robot_interfaces/config/sim_flat_g1lower_cfg.yaml",
     },
     "rl-unitree-sim-g1": {
         "controller": "controllers/config/rl_unitree_g1_cfg.yaml",
