84a2e87fcb067fdc2188c7ffcd50ccc7f9f43a5c
smilesun <smilesun.east@gmail.com>
2026-06-11 09:53:43 +0200

    .

--- src/plans/doom_plans/final/reproduce.sh
+++ src/plans/doom_plans/final/reproduce.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Exported: 2026-06-11 05:30  git: 1f6f800
-# Run dir:  tmp/experiments/go2_contact_20260611_043507_s0
+# Exported: 2026-06-10 19:25  git: 1f6f800
+# Run dir:  tmp/experiments/go2_contact_20260610_165549_s0
 cd "$(git rev-parse --show-toplevel)"
-bash scripts_pcbo/run_go2_contact.sh 0 --config configs_pcbo/go2_contact_gmm_obstacle_lr_5090_lambda1p5_noise0p6.json --seed 0
+bash scripts_pcbo/run_go2_contact.sh 0 --config configs_pcbo/go2_contact_gmm_obstacle_lr_5090_sigma_scheduling.json --seed 0
